9c6bac2278440fe5a72155d5229cbb620d089958
Abdulmohsen alshalabi <76660823+Abdul88dev@users.noreply.github.com>
2022-09-06 12:25:36 +0200

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["abdulcapstone.eu-de.cf.appdomain.cloud","localhost"]
+ALLOWED_HOSTS = ["abdulcapstone.eu-de.mybluemix.net","localhost"]
 
 
 # Application definition
